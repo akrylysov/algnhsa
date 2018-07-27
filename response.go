8ada810ac65cf65a024c707a937a0ce2c634577d
Michael Spiegel <michael.m.spiegel@gmail.com>
2018-07-27 11:36:59 -0400

    Workaround for AWS Gateway multiheaders
    Use uppercase/lowercase variations for http headers
    as a workaround for AWS Gateway not supporting the
    specification for http headers.
    See https://forums.aws.amazon.com/thread.jspa?threadID=205782

--- response.go
+++ response.go
@@ -3,11 +3,39 @@ package algnhsa
 import (
 	"encoding/base64"
 	"net/http/httptest"
+	"unicode"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 const acceptAllContentType = "*/*"
+
+// binaryCase ported from https://github.com/Gi60s/binary-case/blob/b100ba0d63075c28485fd1724d94746f74742107/index.js#L86
+func binaryCase(s string, n int) string {
+	inp := []rune(s)
+	var res []rune
+
+	for i, c := range inp {
+		if n == 0 {
+			res = append(res, inp[i:]...)
+			break
+		}
+		if c <= unicode.MaxASCII && unicode.IsUpper(c) {
+			if n&1 > 0 {
+				c += 32
+			}
+			n >>= 1
+		} else if c <= unicode.MaxASCII && unicode.IsLower(c) {
+			if n&1 > 0 {
+				c -= 32
+			}
+			n >>= 1
+		}
+		res = append(res, c)
+	}
+
+	return string(res)
+}
 
 func newAPIGatewayResponse(w *httptest.ResponseRecorder, binaryContentTypes map[string]bool) (events.APIGatewayProxyResponse, error) {
 	event := events.APIGatewayProxyResponse{}
@@ -18,7 +46,10 @@ func newAPIGatewayResponse(w *httptest.ResponseRecorder, binaryContentTypes map[
 	// Set headers.
 	respHeaders := map[string]string{}
 	for k, v := range w.HeaderMap {
-		respHeaders[k] = v[0]
+		// Workaround for https://forums.aws.amazon.com/thread.jspa?threadID=205782
+		for i, val := range v {
+			respHeaders[binaryCase(k, i)] = val
+		}
 	}
 	event.Headers = respHeaders
 
